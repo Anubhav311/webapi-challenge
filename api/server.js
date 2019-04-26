a85f2ab68ac4e8bdd7f5cd5d43b19868109c19f3
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-26 23:48:08 +0530

    created get and post reqeusts for projects

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require('express')
+const cors = require('cors');
 // const helmet = require('helmet');
 // const morgan = require('morgan');
-// const cors = require('cors');
+// const cors = require('cors')
 
 const projectsRouter = require('./project-router/project');
 const actionsRouter = require('./actions-router/actions');
@@ -10,6 +11,7 @@ const actionsRouter = require('./actions-router/actions');
 const server = express();
 
 server.use(express.json())
+server.use(cors())
 
 server.get('/', (req, res, next) => {
     res.send('its working');
